3581baf5661fd9c0b59ce3c81662cf7dc497eb7c
Katharine McClenaghan <kemcclen@gmail.com>
2023-06-18 14:11:17 -0400

    models files added for Blogpost, index and User

---
+++ models/Blogpost.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Blogpost extends Model {}
+
+Blogpost.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      blog_title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+     
+      blog_post: {
+        type: DataTypes.JSON, 
+        allowNull: false,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id',
+        },
+      }
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'blogpost',
+    }
+  );
+
+module.exports = Blogpost;
