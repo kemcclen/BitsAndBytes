92bf34411deda034004080a7f6dd9add1efd303f
Katharine McClenaghan <kemcclen@gmail.com>
2023-07-31 12:55:48 -0400

    fixed white space at the bottom and altered list-container to expand based on content

--- models/Blogpost.js
+++ models/Blogpost.js
@@ -4,41 +4,40 @@ const sequelize = require('../config/connection');
 class Blogpost extends Model {}
 
 Blogpost.init(
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      blog_title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-     
-      blog_post: {
-        type: DataTypes.STRING, 
-        allowNull: false,
-      },
-      date: {
-        type: DataTypes.DATE,
-        allowNull: false,
-      },
-      user_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'user',
-          key: 'id',
-        },
-      }
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-      sequelize,
-      timestamps: false,
-      freezeTableName: true,
-      underscored: true,
-      modelName: 'blogpost',
-    }
-  );
+    blog_title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    blog_post: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'blogpost',
+  }
+);
 
 module.exports = Blogpost;
